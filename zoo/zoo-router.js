50286c73f6e466f81d9a0e6719e749026029fc6c
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-26 14:51:34 -0500

    This was the easiest assignment in history.

---
+++ zoo/zoo-router.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const knex = require('knex');
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './data/lambda.sqlite3'
+  }
+};
+
+const db = knex(knexConfig);
+
+router.get('/', (req, res) => {
+  // returns a promise that resolves to all records in the table
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('zoos')
+    .where({ id })
+    .first()
+    .then(role => {
+      res.status(200).json(role);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/', (req, res) => {
+  db('zoos')
+    .insert(req.body)
+    .then(ids => {
+      const id = ids[0];
+      db('zoos')
+        .where({ id })
+        .first()
+        .then(role => {
+          res.status(201).json(role);
+        });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'Object not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: 'Object not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
